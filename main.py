88eb2dca67d6ea00283292304b84ef476bc0f2fe
KevinHosey <pelorusjack@protonmail.com>
2024-05-13 14:11:41 +0200

    set back to original script

--- main.py
+++ main.py
@@ -25,23 +25,22 @@ class HelloHandler(http.server.BaseHTTPRequestHandler):
 
             # Query the CallMe service
             callme_url = 'http://gibson.h4ck.me/echo'
-                
-            # callback_url = 'http://{}:{}/hello'.format(self.server.server_address[0], self.server.server_address[1])
-            # payload = {'url': callback_url}
-            # response = requests.post(callme_url, json=payload)
+            callback_url = 'http://{}:{}/hello'.format(self.server.server_address[0], self.server.server_address[1])
+            payload = {'url': callback_url}
+            response = requests.post(callme_url, json=payload)
 
-            # if response.status_code == 200:
-            #     print("Callback successful.")
-            # else:
-            #     print("Callback failed. Status code:", response.status_code)
-            
+            if response.status_code == 200:
+                print("Callback successful.")
+            else:
+                print("Callback failed. Status code:", response.status_code)
+
             # Send response
-           #  self.send_response(200)
-           #  self.end_headers()
-           #  self.wfile.write(json.dumps({"message": "Received and processed"}).encode('utf-8'))
-        # else:
-           #  self.send_response(404)
-           #  self.end_headers()
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps({"message": "Received and processed"}).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.end_headers()
 
 def run(server_class=http.server.HTTPServer, handler_class=HelloHandler, port=8000):
     server_address = ('', port)
